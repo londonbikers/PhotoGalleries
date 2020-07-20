c5cd4448d0b8b80efc89803a169fcf5c0602d6af
Jay Van der Zant <amethi@gmail.com>
2020-07-20 17:31:23 +0100

    Removed unecessary accessor

--- LB.PhotoGalleries.Application/Server.cs
+++ LB.PhotoGalleries.Application/Server.cs
@@ -11,7 +11,7 @@ namespace LB.PhotoGalleries.Application
         /// <summary>
         /// Retrieves the single instance of the application.
         /// </summary>
-        public static Server Instance { get; private set; }
+        public static Server Instance { get; }
         public CategoryServer Categories { get; internal set; }
         public GalleryServer Galleries { get; internal set; }
         #endregion
