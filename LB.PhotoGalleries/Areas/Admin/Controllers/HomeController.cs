ecb4cbfab51f2ee290b551c43d53555274a33e6b
Jay Van der Zant <amethi@gmail.com>
2020-07-20 18:23:36 +0100

    Added support for an admin area

---
+++ LB.PhotoGalleries/Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LB.PhotoGalleries.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles="Administrator,Photographer")]
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- LB.PhotoGalleries/Startup.cs
+++ LB.PhotoGalleries/Startup.cs
@@ -83,6 +83,9 @@ namespace LB.PhotoGalleries
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute(
+                    name: "Admin",
+                    pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
+                endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
             });
