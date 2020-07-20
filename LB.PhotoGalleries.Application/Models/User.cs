7a00ea7b8581414eb26daf88caa141d150caca70
Jay Van der Zant <amethi@gmail.com>
2020-07-20 21:17:19 +0100

    Added json serialiser for data layer use

--- LB.PhotoGalleries.Application/Models/User.cs
+++ LB.PhotoGalleries.Application/Models/User.cs
@@ -1,10 +1,21 @@
-﻿namespace LB.PhotoGalleries.Application.Models
+﻿using System.Text.Json;
+
+namespace LB.PhotoGalleries.Application.Models
 {
     public class User
     {
+        #region accessors
         public string Id { get; set; }
         public string Name { get; set; }
         public string Picture { get; set; }
         public string Email { get; set; }
+        #endregion
+
+        #region public methods
+        public override string ToString()
+        {
+            return JsonSerializer.Serialize(this);
+        }
+        #endregion
     }
 }
