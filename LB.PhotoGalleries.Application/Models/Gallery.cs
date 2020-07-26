aaf8d27a9d70b592f1bcb8392db6ed63552d4e9b
Jay Van der Zant <jay@MacBook-Pro.local>
2020-07-26 09:30:26 +0100

    Adding comments

--- LB.PhotoGalleries.Application/Models/Gallery.cs
+++ LB.PhotoGalleries.Application/Models/Gallery.cs
@@ -13,8 +13,17 @@ namespace LB.PhotoGalleries.Application.Models
         public Guid CategoryId { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
+        /// <summary>
+        /// Inactive photo galleries are not displayed to users.
+        /// </summary>
         public bool Active { get; set; }
-        public Dictionary<int, Image> Images { get;set;}
+        /// <summary>
+        /// The ordered list of images in the photo gallery.
+        /// </summary>
+        public Dictionary<int, Image> Images { get; set;}
+        /// <summary>
+        /// Comments can be made by users against photo galleries themselves as well as on specific photos.
+        /// </summary>
         public List<Comment> Comments { get; set; }
         public string CreatedByUserId { get; set; }
         public DateTime Created { get; set; }
--- LB.PhotoGalleries.Application/Models/Image.cs
+++ LB.PhotoGalleries.Application/Models/Image.cs
@@ -7,7 +7,13 @@ namespace LB.PhotoGalleries.Application.Models
     {
         #region accessors
         public string Name { get; set; }
-        public string Comment { get; set; }
+        /// <summary>
+        /// Descriptive text for the photo
+        /// </summary>
+        public string Caption { get; set; }
+        /// <summary>
+        /// A credit for the photo, i.e. who took it.
+        /// </summary>
         public string Credit { get; set; }
         public DateTime CaptureDate { get; set; }
         public List<Comment> Comments { get; set; }
