7185ea2e2b7072c60f949cc91c957e6eb1cc2fab
Jay Van der Zant <amethi@gmail.com>
2020-07-27 11:40:09 +0100

    Added get/create/update Category functionality

--- LB.PhotoGalleries.Application/Models/Category.cs
+++ LB.PhotoGalleries.Application/Models/Category.cs
@@ -11,7 +11,7 @@ namespace LB.PhotoGalleries.Application.Models
         public string Description { get; set; }
         /// <summary>
         /// Used by CosmosDB to partition container contents to make querying more performant.
-        /// The value should be an arbitary constant as we don't have many categories so all items can reside in the same partition.
+        /// The value should be an arbitrary constant as we don't have many categories so all items can reside in the same partition.
         /// </summary>
         public string PartitionKey { get; set; }
         #endregion
@@ -26,6 +26,17 @@ namespace LB.PhotoGalleries.Application.Models
         public override string ToString()
         {
             return JsonConvert.SerializeObject(this);
+        }
+
+        public bool IsValid()
+        {
+            if (string.IsNullOrEmpty(Id) ||
+                string.IsNullOrEmpty(Name) ||
+                string.IsNullOrEmpty(Description) ||
+                string.IsNullOrEmpty(PartitionKey))
+                return false;
+
+            return true;
         }
         #endregion
     }
--- LB.PhotoGalleries.Application/Servers/CategoryServer.cs
+++ LB.PhotoGalleries.Application/Servers/CategoryServer.cs
@@ -1,13 +1,80 @@
-﻿namespace LB.PhotoGalleries.Application.Servers
+﻿using LB.PhotoGalleries.Application.Models;
+using Microsoft.Azure.Cosmos;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace LB.PhotoGalleries.Application.Servers
 {
     /// <summary>
     /// Provides CRUD functionality for Category objects.
     /// </summary>
     public class CategoryServer
     {
+        #region members
+        private List<Category> _categories;
+        #endregion
+
+        #region accessors
+        public List<Category> Categories
+        {
+            get
+            {
+                if (_categories == null)
+                    LoadCategoriesAsync().Wait();
+
+                return _categories;
+            }
+        }
+        #endregion
+
         #region constructors
         internal CategoryServer()
         {
+        }
+        #endregion
+
+        #region public methods
+        public async Task CreateOrUpdateCategoryAsync(Category category)
+        {
+            if (category == null)
+                throw new InvalidOperationException("Category is null");
+
+            if (!category.IsValid())
+                throw new InvalidOperationException("Category is not valid. Check that all required properties are set");
+
+            var container = Server.Instance.Database.GetContainer(Constants.CategoriesContainerName);
+            var response = await container.UpsertItemAsync(category, new PartitionKey(category.PartitionKey));
+            var createdItem = response.StatusCode == HttpStatusCode.Created;
+            Debug.WriteLine("CategoryServer.CreateOrUpdateCategoryAsync: Created category? " + createdItem);
+
+            // clear the cached categories list so it's retrieved fresh with these latest changes
+            _categories = null;
+        }
+        #endregion
+
+        #region private methods
+        /// <summary>
+        /// Retrieves the categories from the database and caches them for the GET accessor to use.
+        /// </summary>
+        private async Task LoadCategoriesAsync()
+        {
+            _categories = new List<Category>();
+
+            var queryDefinition = new QueryDefinition("SELECT * FROM c");
+            var container = Server.Instance.Database.GetContainer(Constants.CategoriesContainerName);
+            var queryResult = container.GetItemQueryIterator<Category>(queryDefinition);
+
+            while (queryResult.HasMoreResults)
+            {
+                var resultSet = await queryResult.ReadNextAsync();
+                foreach (var category in resultSet)
+                    _categories.Add(category);
+            }
+
+            Debug.WriteLine($"CategoryServer.LoadCategoriesAsync(): Loaded ${_categories.Count} categories from the database.");
         }
         #endregion
     }
