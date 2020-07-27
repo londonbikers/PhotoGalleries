d70c8f422ab59bd999931e84da79707a5b02d270
Jay Van der Zant <amethi@gmail.com>
2020-07-27 18:08:31 +0100

    Added get gallery support

--- LB.PhotoGalleries.Application/Servers/GalleryServer.cs
+++ LB.PhotoGalleries.Application/Servers/GalleryServer.cs
@@ -1,10 +1,39 @@
-﻿namespace LB.PhotoGalleries.Application.Servers
+﻿using LB.PhotoGalleries.Application.Models;
+using Microsoft.Azure.Cosmos;
+using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace LB.PhotoGalleries.Application.Servers
 {
     public class GalleryServer
     {
         #region constructors
         internal GalleryServer()
         {
+        }
+        #endregion
+
+        #region public methods
+        public async Task<Gallery> GetGalleryAsync(string galleryId)
+        {
+            const string query = "SELECT * FROM c WHERE c.Id = '@galleryId'";
+            var queryDefinition = new QueryDefinition(query);
+            queryDefinition.WithParameter("@galleryId", galleryId);
+            var container = Server.Instance.Database.GetContainer(Constants.GalleriesContainerName);
+            var queryResult = container.GetItemQueryIterator<Gallery>(queryDefinition);
+
+            if (queryResult.HasMoreResults)
+            {
+                var resultSet = await queryResult.ReadNextAsync();
+                foreach (var gallery in resultSet)
+                {
+                    Debug.WriteLine("GalleryServer.GetGalleryAsync(): Found a gallery with id: " + galleryId);
+                    return gallery;
+                }
+            }
+
+            throw new InvalidOperationException("No gallery found with id: " + galleryId);
         }
         #endregion
     }
