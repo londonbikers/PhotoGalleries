7f9c066f4871ce56dada2cb01236fb090e433d82
Jay Van der Zant <amethi@gmail.com>
2020-07-27 20:02:09 +0100

    Stub for categories admin

---
+++ LB.PhotoGalleries/Areas/Admin/Controllers/CategoriesController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LB.PhotoGalleries.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles = "Administrator")]
+    public class CategoriesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
