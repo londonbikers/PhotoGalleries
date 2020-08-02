2c5ea93719245024ce0fece0671fb6d349e56d27
Jay Van der Zant <amethi@gmail.com>
2020-08-02 18:00:30 +0100

    Tidyup, remove unecessary scaffold methods

--- LB.PhotoGalleries/Areas/Admin/Controllers/UsersController.cs
+++ LB.PhotoGalleries/Areas/Admin/Controllers/UsersController.cs
@@ -10,68 +10,26 @@ namespace LB.PhotoGalleries.Areas.Admin.Controllers
     [Authorize(Roles = "Administrator")]
     public class UsersController : Controller
     {
-        // GET: UsersController
+        // GET: /admin/users
         public async Task<ActionResult> Index()
         {
             ViewData.Model = await Server.Instance.Users.GetLatestUsersAsync(50);
             return View();
         }
 
-        // GET: UsersController/Details/5
+        // GET: /admin/users/details/5
         public ActionResult Details(int id)
         {
             return View();
         }
 
-        // GET: UsersController/Create
-        public ActionResult Create()
-        {
-            return View();
-        }
-
-        // POST: UsersController/Create
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create(IFormCollection collection)
-        {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: UsersController/Edit/5
-        public ActionResult Edit(int id)
-        {
-            return View();
-        }
-
-        // POST: UsersController/Edit/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, IFormCollection collection)
-        {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: UsersController/Delete/5
+        // GET: /admin/users/delete/5
         public ActionResult Delete(int id)
         {
             return View();
         }
 
-        // POST: UsersController/Delete/5
+        // POST: /admin/users/delete/5
         [HttpPost]
         [ValidateAntiForgeryToken]
         public ActionResult Delete(int id, IFormCollection collection)
