bbea482d472b03d9a6db7afc9fff3a5222b0fa57
Jay Van der Zant <amethi@gmail.com>
2020-08-20 10:22:51 +0100

    Removed unused using statement

--- LB.PhotoGalleries/Areas/Admin/Controllers/ImagesController.cs
+++ LB.PhotoGalleries/Areas/Admin/Controllers/ImagesController.cs
@@ -4,7 +4,6 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Threading.Tasks;
-using LB.PhotoGalleries.Application.Servers;
 
 namespace LB.PhotoGalleries.Areas.Admin.Controllers
 {
