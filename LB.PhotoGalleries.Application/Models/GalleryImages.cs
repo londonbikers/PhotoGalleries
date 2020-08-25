fe8caf0ab4ceb8062cef4998d337286cd47d75cd
Jay Van der Zant <amethi@gmail.com>
2020-08-25 19:12:07 +0100

    Created GalleryImages for future use when caching is in place

---
+++ LB.PhotoGalleries.Application/Models/GalleryImages.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace LB.PhotoGalleries.Application.Models
+{
+    /// <summary>
+    /// Used to contain gallery images so that they can be cached and retrieved easily without having to go to the database each time.
+    /// </summary>
+    public class GalleryImages
+    {
+        public string GalleryId { get; set; }
+        public List<Image> Images { get; set; }
+    }
+}
