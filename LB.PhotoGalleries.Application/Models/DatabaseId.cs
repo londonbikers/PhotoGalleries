cc562b4c1466bfdc68570c1c5709b64d48da1001
Jay Van der Zant <amethi@gmail.com>
2020-08-25 19:14:18 +0100

    Added comments

--- LB.PhotoGalleries.Application/Models/DatabaseId.cs
+++ LB.PhotoGalleries.Application/Models/DatabaseId.cs
@@ -1,5 +1,9 @@
 ﻿namespace LB.PhotoGalleries.Application.Models
 {
+    /// <summary>
+    /// The most efficient way to retrieve an object from the database is to perform a point read.
+    /// To do this a partition key and document id are needed. This object helps encapsulates those for any type of object.
+    /// </summary>
     internal class DatabaseId
     {
         internal string Id { get; set; }
