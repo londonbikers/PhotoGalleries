7a0bbda535994f85c02eb14ac23827afb08b3664
Jay Van der Zant <amethi@gmail.com>
2020-09-12 17:56:38 +0100

    Feature: view component for an image paginator

---
+++ LB.PhotoGalleries/Controllers/Components/ImagePaginatorViewComponent.cs
@@ -0,0 +1,14 @@
+﻿using LB.PhotoGalleries.Application.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace LB.PhotoGalleries.Controllers.Components
+{
+    public class ImagePaginatorViewComponent : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync(PagedResultSet<Image> pagedResultSet)
+        {
+            return View(pagedResultSet);
+        }
+    }
+}
