5e70c8dbcae8f6d86f0974e9a89a475223425c3f
Jay Van der Zant <amethi@gmail.com>
2020-09-15 21:45:00 +0100

    Moving database name to configuration so we can share CosmosDB accounts

--- LB.PhotoGalleries.Application/Constants.cs
+++ LB.PhotoGalleries.Application/Constants.cs
@@ -2,10 +2,6 @@
 {
     public class Constants
     {
-        /// <summary>
-        /// The name of the CosmosDB database to use to store application data.
-        /// </summary>
-        internal static string DatabaseName => "PhotoGalleries";
         internal static string UsersContainerName => "Users";
         internal static string GalleriesContainerName => "Galleries";
         internal static string ImagesContainerName => "Images";
