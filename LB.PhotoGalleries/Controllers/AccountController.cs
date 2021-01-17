d104e1945bbfc473cd8d0a135f6126cf5f1ec12f
Jay Van der Zant <amethi@gmail.com>
2021-01-17 18:06:32 +0000

    Basic user accounts area

---
+++ LB.PhotoGalleries/Controllers/AccountController.cs
@@ -0,0 +1,15 @@
+﻿using LB.PhotoGalleries.Application;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace LB.PhotoGalleries.Controllers
+{
+    public class AccountController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            ViewData.Model = await Server.Instance.Users.GetUserAsync(Helpers.GetUserId(User));
+            return View();
+        }
+    }
+}
