33e0184845603fa925e156f7c6eb6973f4c89925
Jay Van der Zant <jay@tetron.io>
2021-05-22 14:10:03 +0100

    Presentation refinements

--- LB.PhotoGalleries.ComparisonTool/Program.cs
+++ LB.PhotoGalleries.ComparisonTool/Program.cs
@@ -88,9 +88,10 @@ namespace LB.PhotoGalleries.ComparisonTool
                 Parallel.ForEach(files, file =>
                 {
                     var table = new Table();
+                    table.Title(file);
                     table.AddColumn("Image File Spec");
-                    table.AddColumn("Generation Time (ms)");
-                    table.AddColumn("Byte Size");
+                    table.AddColumn("Time (ms)");
+                    table.AddColumn("Size (kb)");
 
                     ProcessInputFileAsync(file, specs, outputPath, table, overallProgress).GetAwaiter().GetResult();
                     AnsiConsole.Render(table);
@@ -107,44 +108,72 @@ namespace LB.PhotoGalleries.ComparisonTool
             {
                 // lower quality, higher efficiency, max support
                 new ImageFileSpec(FileSpec.SpecLowRes, FileSpecFormat.Jpeg, 400, 25, 0f, FileSpec.SpecLowRes.ToString()),
-                new ImageFileSpec(FileSpec.Spec800, FileSpecFormat.Jpeg, 800, 50, 0f, FileSpec.Spec800.ToString()),
-                new ImageFileSpec(FileSpec.Spec1920, FileSpecFormat.Jpeg, 1920, 50, 0f, FileSpec.Spec1920.ToString()),
-                new ImageFileSpec(FileSpec.Spec2560, FileSpecFormat.Jpeg, 2560, 50, 0f, FileSpec.Spec2560.ToString()),
-                new ImageFileSpec(FileSpec.Spec3840, FileSpecFormat.Jpeg, 3840, 50, 0f, FileSpec.Spec3840.ToString()),
-
                 // low quality, high efficiency, max support
                 new ImageFileSpec(FileSpec.SpecLowRes, FileSpecFormat.Jpeg, 400, 50, 0f, FileSpec.SpecLowRes.ToString()),
-                new ImageFileSpec(FileSpec.Spec800, FileSpecFormat.Jpeg, 800, 70, 0f, FileSpec.Spec800.ToString()),
-                new ImageFileSpec(FileSpec.Spec1920, FileSpecFormat.Jpeg, 1920, 70, 0f, FileSpec.Spec1920.ToString()),
-                new ImageFileSpec(FileSpec.Spec2560, FileSpecFormat.Jpeg, 2560, 70, 0f, FileSpec.Spec2560.ToString()),
-                new ImageFileSpec(FileSpec.Spec3840, FileSpecFormat.Jpeg, 3840, 70, 0f, FileSpec.Spec3840.ToString()),
-
                 // medium quality, medium efficiency, max support
                 new ImageFileSpec(FileSpec.SpecLowRes, FileSpecFormat.Jpeg, 400, 75, 0f, FileSpec.SpecLowRes.ToString()),
-                new ImageFileSpec(FileSpec.Spec800, FileSpecFormat.Jpeg, 800, 80, 0f, FileSpec.Spec800.ToString()),
-                new ImageFileSpec(FileSpec.Spec1920, FileSpecFormat.Jpeg, 1920, 80, 0f, FileSpec.Spec1920.ToString()),
-                new ImageFileSpec(FileSpec.Spec2560, FileSpecFormat.Jpeg, 2560, 80, 0f, FileSpec.Spec2560.ToString()),
-                new ImageFileSpec(FileSpec.Spec3840, FileSpecFormat.Jpeg, 3840, 80, 0f, FileSpec.Spec3840.ToString()),
-
                 // high quality, medium efficiency, max support
                 new ImageFileSpec(FileSpec.SpecLowRes, FileSpecFormat.Jpeg, 400, 80, 0f, FileSpec.SpecLowRes.ToString()),
-                new ImageFileSpec(FileSpec.Spec800, FileSpecFormat.Jpeg, 800, 90, 0f, FileSpec.Spec800.ToString()),
-                new ImageFileSpec(FileSpec.Spec1920, FileSpecFormat.Jpeg, 1920, 90, 0f, FileSpec.Spec1920.ToString()),
-                new ImageFileSpec(FileSpec.Spec2560, FileSpecFormat.Jpeg, 2560, 90, 0f, FileSpec.Spec2560.ToString()),
-                new ImageFileSpec(FileSpec.Spec3840, FileSpecFormat.Jpeg, 3840, 90, 0f, FileSpec.Spec3840.ToString()),
-
                 // very high quality, low efficiency, max support
                 new ImageFileSpec(FileSpec.SpecLowRes, FileSpecFormat.Jpeg, 400, 90, 0f, FileSpec.SpecLowRes.ToString()),
-                new ImageFileSpec(FileSpec.Spec800, FileSpecFormat.Jpeg, 800, 100, 0f, FileSpec.Spec800.ToString()),
-                new ImageFileSpec(FileSpec.Spec1920, FileSpecFormat.Jpeg, 1920, 100, 0f, FileSpec.Spec1920.ToString()),
-                new ImageFileSpec(FileSpec.Spec2560, FileSpecFormat.Jpeg, 2560, 100, 0f, FileSpec.Spec2560.ToString()),
-                new ImageFileSpec(FileSpec.Spec3840, FileSpecFormat.Jpeg, 3840, 100, 0f, FileSpec.Spec3840.ToString()),
-
                 // high quality, high efficiency, lower support
                 new ImageFileSpec(FileSpec.SpecLowRes, FileSpecFormat.WebP, 400, 75, 0f, FileSpec.SpecLowRes.ToString()),
+
+
+                // lower quality, higher efficiency, max support
+                new ImageFileSpec(FileSpec.Spec800, FileSpecFormat.Jpeg, 800, 50, 0f, FileSpec.Spec800.ToString()),
+                // low quality, high efficiency, max support
+                new ImageFileSpec(FileSpec.Spec800, FileSpecFormat.Jpeg, 800, 70, 0f, FileSpec.Spec800.ToString()),
+                // medium quality, medium efficiency, max support
+                new ImageFileSpec(FileSpec.Spec800, FileSpecFormat.Jpeg, 800, 80, 0f, FileSpec.Spec800.ToString()),
+                // high quality, medium efficiency, max support
+                new ImageFileSpec(FileSpec.Spec800, FileSpecFormat.Jpeg, 800, 90, 0f, FileSpec.Spec800.ToString()),
+                // very high quality, low efficiency, max support
+                new ImageFileSpec(FileSpec.Spec800, FileSpecFormat.Jpeg, 800, 100, 0f, FileSpec.Spec800.ToString()),
+                // high quality, high efficiency, lower support
                 new ImageFileSpec(FileSpec.Spec800, FileSpecFormat.WebP, 800, 90, 0f, FileSpec.Spec800.ToString()),
+
+
+
+                // lower quality, higher efficiency, max support
+                new ImageFileSpec(FileSpec.Spec1920, FileSpecFormat.Jpeg, 1920, 50, 0f, FileSpec.Spec1920.ToString()),
+                // low quality, high efficiency, max support
+                new ImageFileSpec(FileSpec.Spec1920, FileSpecFormat.Jpeg, 1920, 70, 0f, FileSpec.Spec1920.ToString()),
+                // medium quality, medium efficiency, max support
+                new ImageFileSpec(FileSpec.Spec1920, FileSpecFormat.Jpeg, 1920, 80, 0f, FileSpec.Spec1920.ToString()),
+                // high quality, medium efficiency, max support
+                new ImageFileSpec(FileSpec.Spec1920, FileSpecFormat.Jpeg, 1920, 90, 0f, FileSpec.Spec1920.ToString()),
+                // very high quality, low efficiency, max support
+                new ImageFileSpec(FileSpec.Spec1920, FileSpecFormat.Jpeg, 1920, 100, 0f, FileSpec.Spec1920.ToString()),
+                // high quality, high efficiency, lower support
                 new ImageFileSpec(FileSpec.Spec1920, FileSpecFormat.WebP, 1920, 90, 0f, FileSpec.Spec1920.ToString()),
+
+
+                // lower quality, higher efficiency, max support
+                new ImageFileSpec(FileSpec.Spec2560, FileSpecFormat.Jpeg, 2560, 50, 0f, FileSpec.Spec2560.ToString()),
+                // low quality, high efficiency, max support
+                new ImageFileSpec(FileSpec.Spec2560, FileSpecFormat.Jpeg, 2560, 70, 0f, FileSpec.Spec2560.ToString()),
+                // medium quality, medium efficiency, max support
+                new ImageFileSpec(FileSpec.Spec2560, FileSpecFormat.Jpeg, 2560, 80, 0f, FileSpec.Spec2560.ToString()),
+                // high quality, medium efficiency, max support
+                new ImageFileSpec(FileSpec.Spec2560, FileSpecFormat.Jpeg, 2560, 90, 0f, FileSpec.Spec2560.ToString()),
+                // very high quality, low efficiency, max support
+                new ImageFileSpec(FileSpec.Spec2560, FileSpecFormat.Jpeg, 2560, 100, 0f, FileSpec.Spec2560.ToString()),
+                // high quality, high efficiency, lower support
                 new ImageFileSpec(FileSpec.Spec2560, FileSpecFormat.WebP, 2560, 90, 0f, FileSpec.Spec2560.ToString()),
+
+
+                // lower quality, higher efficiency, max support
+                new ImageFileSpec(FileSpec.Spec3840, FileSpecFormat.Jpeg, 3840, 50, 0f, FileSpec.Spec3840.ToString()),
+                // low quality, high efficiency, max support
+                new ImageFileSpec(FileSpec.Spec3840, FileSpecFormat.Jpeg, 3840, 70, 0f, FileSpec.Spec3840.ToString()),
+                // medium quality, medium efficiency, max support
+                new ImageFileSpec(FileSpec.Spec3840, FileSpecFormat.Jpeg, 3840, 80, 0f, FileSpec.Spec3840.ToString()),
+                // high quality, medium efficiency, max support
+                new ImageFileSpec(FileSpec.Spec3840, FileSpecFormat.Jpeg, 3840, 90, 0f, FileSpec.Spec3840.ToString()),
+                // very high quality, low efficiency, max support
+                new ImageFileSpec(FileSpec.Spec3840, FileSpecFormat.Jpeg, 3840, 100, 0f, FileSpec.Spec3840.ToString()),
+                // high quality, high efficiency, lower support
                 new ImageFileSpec(FileSpec.Spec3840, FileSpecFormat.WebP, 3840, 90, 0f, FileSpec.Spec3840.ToString())
             };
         }
@@ -153,7 +182,10 @@ namespace LB.PhotoGalleries.ComparisonTool
         {
             var inputImageStopwatch = new Stopwatch();
             inputImageStopwatch.Start();
-            var fileBytes = await File.ReadAllBytesAsync(file);
+            var inputFileBytes = await File.ReadAllBytesAsync(file);
+            var inputFileKb = inputFileBytes.Length / 1024;
+
+            table.Caption($"Input image was {inputFileKb} kb in size.");
 
             // each input file should have it's own folder to make navigation and comparison easier
             var inputFilenameWithoutExtension = Path.GetFileNameWithoutExtension(file);
@@ -162,7 +194,7 @@ namespace LB.PhotoGalleries.ComparisonTool
 
             Parallel.ForEach(specs, spec =>
             {
-                ProcessImageFileSpecAsync(fileBytes, spec, inputImageOutputDirectory, table, overallProgress).GetAwaiter().GetResult();
+                ProcessImageFileSpecAsync(inputFileBytes, spec, inputImageOutputDirectory, table, overallProgress).GetAwaiter().GetResult();
             });
 
             inputImageStopwatch.Stop();
