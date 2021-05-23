6e5069897169ca2b1b4709ba0b45d3e57cf0971d
Jay Van der Zant <jay@tetron.io>
2021-05-23 18:06:31 +0100

    Removed outdated comment

--- LB.PhotoGalleries/Startup.cs
+++ LB.PhotoGalleries/Startup.cs
@@ -122,7 +122,6 @@ namespace LB.PhotoGalleries
             {
                 // store processed image files to local storage to use as a cache
                 // for development just create a local folder and reference that in configuration.
-                // for production we intend on using local Azure App Service storage (d:\local). This is ephemeral but free!
                 InitialiseImageFlowDiskCache();
                 services.AddImageflowDiskCache(new DiskCacheOptions(Configuration["ImageFlow:DiskCachePath"]));
             }
