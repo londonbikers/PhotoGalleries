c12705de0a414f5d977dbd6b913fd8976de6a03e
Jay Van der Zant <jay@tetron.io>
2021-05-24 19:45:44 +0100

    Reverting to webp 80q 25s

--- LB.PhotoGalleries.Shared/ImageFileSpecs.cs
+++ LB.PhotoGalleries.Shared/ImageFileSpecs.cs
@@ -10,12 +10,11 @@ namespace LB.PhotoGalleries.Shared
     {
         public static List<ImageFileSpec> Specs => new List<ImageFileSpec>
         {
-            new ImageFileSpec(FileSpec.SpecOriginal, "originals"),
-            new ImageFileSpec(FileSpec.Spec3840, FileSpecFormat.Jpeg, 3840, 90, 15f, InterpolationFilter.Mitchell, FileSpec.Spec3840.ToString().ToLower()),
-            new ImageFileSpec(FileSpec.Spec2560, FileSpecFormat.Jpeg, 2560, 90, 15f, InterpolationFilter.Mitchell, FileSpec.Spec2560.ToString().ToLower()),
-            new ImageFileSpec(FileSpec.Spec1920, FileSpecFormat.Jpeg, 1920, 90, 15f, InterpolationFilter.Mitchell, FileSpec.Spec1920.ToString().ToLower()),
-            new ImageFileSpec(FileSpec.Spec800, FileSpecFormat.Jpeg, 800, 90, 15f, InterpolationFilter.Mitchell, FileSpec.Spec800.ToString().ToLower()),
-            new ImageFileSpec(FileSpec.SpecLowRes, FileSpecFormat.Jpeg, 400, 25, "lowres")
+            new ImageFileSpec(FileSpec.Spec3840, FileSpecFormat.WebPLossy, 3840, 80, 25f, InterpolationFilter.Mitchell, FileSpec.Spec3840.ToString().ToLower()),
+            new ImageFileSpec(FileSpec.Spec2560, FileSpecFormat.WebPLossy, 2560, 80, 25f, InterpolationFilter.Mitchell, FileSpec.Spec2560.ToString().ToLower()),
+            new ImageFileSpec(FileSpec.Spec1920, FileSpecFormat.WebPLossy, 1920, 80, 25f, InterpolationFilter.Mitchell, FileSpec.Spec1920.ToString().ToLower()),
+            new ImageFileSpec(FileSpec.Spec800, FileSpecFormat.WebPLossy, 800, 80, 25f, InterpolationFilter.Mitchell, FileSpec.Spec800.ToString().ToLower()),
+            new ImageFileSpec(FileSpec.SpecLowRes, FileSpecFormat.WebPLossy, 400, 25, "lowres")
         };
 
         public static ImageFileSpec GetImageFileSpec(FileSpec fileSpec)
