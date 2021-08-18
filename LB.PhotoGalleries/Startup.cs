453cdfe97eaed7d80eb78a0654ff276356e79b7b
Jay Van der Zant <jay@Akitha.local>
2021-08-18 17:31:35 +0100

    Got rid of /Home from base URLs

--- LB.PhotoGalleries/Startup.cs
+++ LB.PhotoGalleries/Startup.cs
@@ -137,7 +137,7 @@ namespace LB.PhotoGalleries
             }
             else
             {
-                app.UseExceptionHandler("/Home/Error");
+                app.UseExceptionHandler("/error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
@@ -199,6 +199,10 @@ namespace LB.PhotoGalleries
                 endpoints.MapControllerRoute(
                     name: "ImageTag",
                     pattern: "t/{tag}", new { controller = "Tags", action = "Details" });
+                endpoints.MapControllerRoute(
+                    name: "Home",
+                    pattern: "{action}",
+                    new { action = "Index", controller = "Home" });
                 endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
